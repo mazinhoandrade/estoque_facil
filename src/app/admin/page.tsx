5a4538f0524ea02caf88b7e5219e2d9795ec2076
mazinhoandrade <irismarandrade94@gmail.com>
2025-03-27 11:50:43 -0300

    feat: inicio da criação do componente dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,8 @@ import { Dashboard } from "@/src/components/dashboard";
 
 export const Home = () => {
   return (
-    <main className="min-h-screen items-center">
+    <main className="min-h-screen items-center mb-20">
+      <h1 className="mx-2 text-2xl font-bold">Dashboard</h1>
       <Dashboard />
     </main>
   );
--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,13 +1,29 @@
+import { ListCheck, TrendingUp } from "lucide-react"
+
+import { Graphic } from "@/components/graphic";
 import { Card } from "@/components/ui/card";
 
 export const Dashboard = () => {
   return (
-    <Card className="mt-4 w-full px-2">
-      <div className="grid w-full grid-cols-2 gap-4">
-        <div className="text-center text-2xl">9 produtos</div>
-        <div className="text-center text-2xl">5 vendas</div>
+    <div className="mt-4 w-full px-2 space-y-2">
+      <div className="grid-cols grid w-full gap-4 text-2xl text-white lg:grid-cols-2">
+        <Card className="bg-primary py-5 text-center">9 produtos</Card>
+        <Card className="bg-primary py-5 text-center">5 vendas</Card>
       </div>
-      <div className="mt-4 text-center text-2xl">produtos mais vendidos</div>
-    </Card>
+      <div className="grid-cols grid w-full gap-4 text-left text-xl lg:grid-cols-2">
+        <div className="mt-4">
+            <h1 className="flex items-center">Top 10 Produtos Mais Vendidos <ListCheck color={"green"} /></h1>
+            <Card className="bg-primary py-2 text-center flex justify-between text-white px-1 items-center"><div>bola</div><div className="text-xs">10 vendas</div></Card>
+        </div>
+        <div className="mt-4">
+            <h1 className="flex items-center">Top 10 Produtos com estoque baixo <ListCheck color="red"  /></h1>
+            <Card className="bg-primary py-2 text-center flex justify-between items-center text-white px-1"><div>bola</div><div className="text-xs">Resta 4 unidades</div></Card>
+        </div>
+      </div>
+      <div className="w-full">
+          <h1 className="flex items-center">Grafico de vendas <TrendingUp /></h1>
+          <Graphic />
+        </div>
+    </div>
   );
 };
